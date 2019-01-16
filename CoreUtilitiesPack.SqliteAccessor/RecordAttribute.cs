69de15f537deb415cb06fab4f14fa8a15c076a47
kota_na_ <ad.maiora.1@gmail.com>
2019-01-16 23:09:48 +0900

    機能追加 インメモリDBが利用できないのでデフォルトのデータソースをファイルに変更 テーブル作成SQLの取得 Dictionary、クラス変換

---
+++ CoreUtilitiesPack.SqliteAccessor/RecordAttribute.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace CoreUtilitiesPack
+{
+    /// <summary>
+    /// DBのカラム属性
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Property)]
+    public class RecordAttribute : Attribute
+    {
+        public enum FieldType { NONE, NULL, INTEGER, REAL, TEXT, BLOB };
+
+        /// <summary>
+        /// カラムの名称
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// カラムのデータ形式
+        /// </summary>
+        public FieldType Type { get; set; }
+
+        /// <summary>
+        /// 主キー設定
+        /// </summary>
+        public bool Primary { get; set; }
+
+        /// <summary>
+        /// ユニーク制約設定
+        /// </summary>
+        public bool Unique { get; set; }
+
+        public RecordAttribute()
+        {
+            Name = "";
+            Type = FieldType.NONE;
+            Primary = false;
+            Unique = false;
+        }
+    }
+}
--- CoreUtilitiesPack.SqliteAccessor/SqliteAccessor.cs
+++ CoreUtilitiesPack.SqliteAccessor/SqliteAccessor.cs
@@ -7,7 +7,7 @@ namespace CoreUtilitiesPack
 {
     public class SqliteAccessor
     {
-        private string connectionString = @"Data Source=:memory:;";
+        private string connectionString = @"Data Source=default.db;";
 
         /// <summary>
         /// SQLを実行する
@@ -26,7 +26,6 @@ namespace CoreUtilitiesPack
                 cmd.CommandText = sql;
                 result = cmd.ExecuteNonQuery();
                 conn.Close();
-
             }
 
             return result;
@@ -159,6 +158,131 @@ namespace CoreUtilitiesPack
                 return true;
             }
             return false;
+        }
+
+        /// <summary>
+        /// Dictionaryをクラスに変換する
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="dictionary"></param>
+        /// <param name="record"></param>
+        public void ToRecord<T>(Dictionary<string, object> dictionary, out T record)
+        {
+            record = (T)Activator.CreateInstance(typeof(T));
+
+            var properties = typeof(T).GetProperties(BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic);
+            foreach (var property in properties)
+            {
+                var attribute = property.GetCustomAttribute(typeof(RecordAttribute)) as RecordAttribute;
+                if (attribute == null) { continue; }
+
+                if (dictionary.ContainsKey(attribute.Name))
+                {
+                    property.SetValue(record, Convert.ChangeType(dictionary[attribute.Name], property.PropertyType));
+                }
+            }
+        }
+
+        /// <summary>
+        /// Dictionaryをクラスに変換する
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="dictionaries"></param>
+        /// <param name="records"></param>
+        public void ToRecords<T>(List<Dictionary<string, object>> dictionaries, out List<T> records)
+        {
+            records = new List<T>();
+            foreach (var dictionary in dictionaries)
+            {
+                var record = (T)Activator.CreateInstance(typeof(T));
+                ToRecord(dictionary, out record);
+                records.Add(record);
+            }
+        }
+
+        /// <summary>
+        /// クラスをDictionaryに変換する
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="record"></param>
+        /// <param name="dictionary"></param>
+        public void ToDictionary<T>(T record, out Dictionary<string, object> dictionary)
+        {
+            dictionary = new Dictionary<string, object>();
+            var properties = typeof(T).GetProperties(BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic);
+            foreach (var property in properties)
+            {
+                var attribute = property.GetCustomAttribute(typeof(RecordAttribute)) as RecordAttribute;
+                if (attribute == null) { continue; }
+                dictionary.Add(attribute.Name, property.GetValue(record));
+            }
+        }
+
+        /// <summary>
+        /// クラスをDictionaryに変換する
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="records"></param>
+        /// <param name="dictionaries"></param>
+        public void ToDictionaries<T>(List<T> records, out List<Dictionary<string, object>> dictionaries)
+        {
+            dictionaries = new List<Dictionary<string, object>>();
+            foreach (var record in records)
+            {
+                var dictionary = new Dictionary<string, object>();
+                ToDictionary(record, out dictionary);
+                dictionaries.Add(dictionary);
+            }
+        }
+
+        /// <summary>
+        /// テーブル作成用のSQLを取得する
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="tableName"></param>
+        /// <param name="record"></param>
+        /// <returns>SQL文</returns>
+        public static string GetCreateTableSQL<T>(string tableName, T record)
+        {
+            var properties = typeof(T).GetProperties(BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic);
+            var primaryKeys = new List<string>();
+            var fields = new List<string>();
+
+            var sql = "create table " + tableName + " (";
+            foreach (var property in properties)
+            {
+                var attribute = property.GetCustomAttribute(typeof(RecordAttribute)) as RecordAttribute;
+                if (attribute == null) { continue; }
+
+                if (attribute.Type == RecordAttribute.FieldType.NONE)
+                {
+                    sql += attribute.Name + ",";
+                }
+                else
+                {
+                    sql += attribute.Name + " " + attribute.Type + ",";
+                }
+
+                if (attribute.Primary)
+                {
+                    primaryKeys.Add(attribute.Name);
+                }
+                fields.Add(attribute.Name);
+            }
+
+            sql += "PRIMARY KEY (";
+            if (primaryKeys.Count != 0)
+            {
+                sql += string.Join(", ", primaryKeys);
+            }
+            else
+            {
+                sql += string.Join(", ", fields);
+
+            }
+            sql += "))";
+
+            return sql;
         }
     }
 }
